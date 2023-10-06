7be7ff230e9800c176202287ebd01cd33f8ce5c8
Sara Vesterinen <sara.vesterinen@hotmail.com>
2023-10-06 22:30:40 +0300

    playRound created

--- scripts.js
+++ scripts.js
@@ -12,8 +12,26 @@ function getComputerChoice(ran){
     }
 }
 
+let computerSelection = getComputerChoice(ran);
 let playerSelection = prompt("Choose rock, paper or scissors: ").toLowerCase();
 
 function playRound(playerSelection, computerSelection){
-
+    if(playerSelection === computerSelection){
+        return "It's a tie!";
+    }
+    else if(playerSelection == "rock" && computerSelection == "scissors" ||
+    playerSelection == "paper" && computerSelection == "rock" ||
+    playerSelection == "scissors" && computerSelection == "paper"){
+        return "You win! " + playerSelection + " wins " + computerSelection;
+    }
+    else if(computerSelection == "rock" && playerSelection == "scissors" ||
+    computerSelection == "paper" && playerSelection == "rock" ||
+    computerSelection == "scissors" && playerSelection == "paper"){
+        return "You loose! " + computerSelection + " wins " + playerSelection;
+    }
+    else{
+        return "Something went wrong :(";
+    }
 }
+
+console.log(playRound(playerSelection, computerSelection));
